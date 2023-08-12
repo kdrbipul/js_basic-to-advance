96c266f3b267f04080d7085357ec91ef1a9ebb8c
kdrbipul <kdrbipul@gmail.com>
2023-08-13 01:37:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+
+// var firstName ="Kader";
+// console.log(firstName);
+
+// firstName="Bipul";
+// console.log(firstName);
+
+// let firstName="Kader";
+// firstNeme="Bipul";
+// console.log(firstName);
+
+// const pi=3.14;
+// pi=3.15;
+// console.log(pi);
+
+// const firstName="Abdul Kader";
+// console.log(firstName.length);
+// console.log(firstName[firstName.length-1])
+
+// let firstName="   Abdul Kader   ";
+// console.log(firstName.length);
+// let newString=firstName.trim();
+// console.log(newString);
+// console.log(newString.length);
+// console.log(firstName.toUpperCase());
+
+let firstName="Abdul Kader";
+let newString=firstName.slice(1,5);
+console.log(newString);

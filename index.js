38a3bf63554036db6c9c11509765a80e136cf0c9
kdrbipul <kdrbipul@gmail.com>
2023-08-14 00:05:52 +0600

    upto date

--- index.js
+++ index.js
@@ -50,7 +50,45 @@
 // let fullString= string3 + " " +string4;
 // console.log(fullString);
 
-let age=23;
-let name="Abdul Kader";
-let aboutMe="My name is"+" "+ name +" " +"and my age is" + " "+age;
-console.log(aboutMe); // This called is template string;
+// let age=23;
+// let name="Abdul Kader";
+// let aboutMe="My name is"+" "+ name +" " +"and my age is" + " "+age;
+// console.log(aboutMe); // This called is template string;
+
+
+// let aboutMe=`My name is ${name} and my age is ${age}`;
+// console.log(aboutMe);
+
+
+// let firstName1;
+// console.log(firstName1);
+
+// var firstName2;
+// console.log(firstName2);
+
+// const firstName3;
+// console.log(firstName3);
+
+// Parameter in decimal format
+// let bigNum = BigInt(
+//     "123422222222222222222222222222222222222");
+//     console.log(bigNum);
+    
+//     // Parameter in hexadecimal format
+//     let bigHex = BigInt("0x1ffffffeeeeeeeeef");
+//     console.log(bigHex);
+    
+//     // Parameter in binary format
+//     let bigBin = BigInt(
+//     "0b1010101001010101001111111111111111");
+//     console.log(bigBin);
+
+// let number=BigInt(123);
+// let sameMyNumber=12n;
+// console.log(number + sameMyNumber);
+
+let num1 = 13;
+let num2 = 15;
+let num3 = num1>num2;
+console.log(num1>num2);
+    
